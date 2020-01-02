07fff98811032071e3b85aa5a41dfb41d87bb423
Vijay <Vijay@QA1.Johnbhai.lan>
2020-01-02 16:16:37 +0530

    Tes

---
+++ Java Project/src/GridProject/printHello.java
@@ -0,0 +1,19 @@
+package GridProject;
+
+ 
+
+public class printHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 
+				System.out.println("jenkins Test completed ") ;
+				
+			}
+		     
+		 
+
+	}
+
+ 
